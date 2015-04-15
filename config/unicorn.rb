9b7598116684e8088ec9abe7941826ea8c8a50aa
Scott Miller <scott.miller.utah@gmail.com>
2015-04-15 16:35:04 -0600

    Update Unicorn config
    Signed-off-by: Scott Miller <scott.miller.utah@gmail.com>

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -10,3 +10,24 @@ preload_app true
 # Immediately restart any workers that
 # haven't responded within 30 seconds
 timeout 30
+
+before_fork do |server, worker|
+
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
