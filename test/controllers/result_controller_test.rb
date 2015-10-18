50f4813070edeff3fd5a687dfcc59d344da69650
Scott Miller <scott.miller.utah@gmail.com>
2015-10-18 17:14:58 -0600

    Bug fixes: Fixes #6, Fixes #12, Added to test coverage for results
    Signed-off-by: Scott Miller <scott.miller.utah@gmail.com>

--- test/controllers/result_controller_test.rb
+++ test/controllers/result_controller_test.rb
@@ -10,7 +10,6 @@ class ResultControllerTest < ActionController::TestCase
 
   end
 
-
   test "a browsing user searching for a doc should return a result" do
 
     get(:index, { q: 'article1 text' })
@@ -30,6 +29,7 @@ class ResultControllerTest < ActionController::TestCase
     get(:index, { q: 'This is a public post' })
     assert_not_nil assigns(:results)
     assert_operator assigns(:results).total_count, :>=, 1, "Did not find at least one result"
+    assert_select 'span.result-body', { text: "This is a public post, should be searchable" }
     assert_response :success
   end
 
@@ -53,6 +53,7 @@ class ResultControllerTest < ActionController::TestCase
     get(:index, { q: 'This is something amazing' })
     assert_not_nil assigns(:results)
     assert_equal(1, assigns(:results).total_count, "Did not find results for the search")
+    assert_select 'span.result-body', { text: "This is something amazing" }
     assert_response :success
 
   end
@@ -67,6 +68,32 @@ class ResultControllerTest < ActionController::TestCase
 
     get(:index, { q: 'This is something private' })
     assert_equal(0, assigns(:results).total_count, "Found results for the search when shouldn't have")
+    assert_response :success
+
+  end
+
+  test "a browing user shoud be able to search and find a newly created article" do
+    @doc = Doc.create(category_id: 1, title: "some title", body: 'some body text')
+
+    # have to manually rebuild search
+    PgSearch::Multisearch.rebuild(Doc)
+
+    get(:index, { q: 'some body text' })
+    assert_not_nil assigns(:results)
+    assert_equal(1, assigns(:results).total_count, "Did not find results for the search for a new created Doc")
+    assert_response :success
+
+  end
+
+  test "a browing user shoud not see HTML code in the search results" do
+    @doc = Doc.create(user_id: 1, category_id: 1, title: "new title", body: '<div><b>new</b> body text</div>')
+
+    # have to manually rebuild search
+    PgSearch::Multisearch.rebuild(Doc)
+
+    get(:index, { q: 'new body text' })
+    assert_not_nil assigns(:results)
+    assert_select 'span.result-body', { text: "new body text" }
     assert_response :success
 
   end
