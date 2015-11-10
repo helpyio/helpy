b55d999699378127fe94fef875532b3afebd4bbe
Scott Miller <scott.miller.utah@gmail.com>
2015-11-09 20:45:33 -0700

    Update select common reply helper with hack to handle missing translations for chinese char sets
    Signed-off-by: Scott Miller <scott.miller.utah@gmail.com>

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -11,13 +11,23 @@ module AdminHelper
 
   def i18n_reply
 
+    # Builds the nested selector for common replies
+
     select = "<label class='control-label' for='post_reply_id'>#{t(:select_common, default: 'Insert Common Reply')}</label>"
     select += "<select name='post[reply_id]' class='form-control' id='post_reply_id'>"
     select += "<option value=''></option>"
 
     I18n.available_locales.each do |locale|
       Globalize.with_locale(locale) do
-        select += "<optgroup label='#{I18n.translate("i18n_languages.#{locale}")}'>"
+        # TODO THIS IS A HACK because there appears to be no difference in language files for chinese simple and traditional
+        # This could be changed to display the language names in english fairly easily
+        # but in another language we are missing the translations
+
+        if "#{locale}" == 'zh-cn' || "#{locale}" == 'zh-tw'
+          select += "<optgroup label='#{I18n.translate("i18n_languages.zh")}'>"
+        else
+          select += "<optgroup label='#{I18n.translate("i18n_languages.#{locale}")}'>"
+        end
         Doc.replies.with_translations(locale).all.each do |doc|
           select += "<option value='#{doc.body}'>#{doc.title}</option>"
         end
