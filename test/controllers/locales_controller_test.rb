b37b03d6d910d711a877d3fc420fa284f1c67d51
Scott Miller <scott.miller.utah@gmail.com>
2015-11-10 20:19:03 -0700

    Updated french locale, started working on some locale controller tests
    Signed-off-by: Scott Miller <scott.miller.utah@gmail.com>

--- test/controllers/locales_controller_test.rb
+++ test/controllers/locales_controller_test.rb
@@ -1,7 +1,42 @@
 require 'test_helper'
 
 class LocalesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def one_locale
+    config.i18n.default_locale = :en
+    config.i18n.available_locales = ['en']
+  end
+
+  def multi_locale
+    config.i18n.default_locale = :fr
+    config.i18n.available_locales = [:en, :fr, :et]
+  end
+
+
+  test "a browsing user requesting the root domain should get redirected to the default language" do
+    one_locale
+
+    get :redirect_on_locale do
+      assert_redirected_to(controller: 'home', action: 'index', locale: :en)
+    end
+  end
+
+  test "a browsing user requesting the root domain should get redirected to the default language when multiple languages are set" do
+    multi_locale
+
+    get :redirect_on_locale do
+      assert_redirected_to(controller: 'home', action: 'index', locale: :fr)
+    end
+  end
+
+  test "a browsing user requesting the root domain should get redirected to the browser language" do
+    multi_locale
+
+    @request.headers["Accept-Lanuage"] = "et"
+    get :redirect_on_locale do
+      assert_redirected_to(controller: 'home', action: 'index', locale: :et)
+    end
+  end
+
+
 end
