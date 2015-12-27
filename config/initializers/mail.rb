59d09c1ec540693a275c84fc7a7e88b473443ba4
Scott Miller <scott.miller.utah@gmail.com>
2015-12-27 14:52:03 -0700

    Used wrong key for send grid api

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,6 +2,6 @@ ActionMailer::Base.smtp_settings = {
     :address   => "smtp.sendgrid.net",
     :port      => 587,
     :user_name => Settings.mail_username,
-    :password  => Settings.mail_key,
+    :password  => Settings.mail_api_key,
     :domain    => 'heroku.com'
   }
