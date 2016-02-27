ceab0f5619093befd234164599fc980cbc132beb
Ollie Bennett <olliebennett@gmail.com>
2016-02-27 01:36:44 +0000

    Deliberately slow development database queries
    In order to highlight inefficient queries, and N+1
    problems etc. I find it useful to force myself to
    experience the slowness directly!
    
    This change disables caching of results from the database.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,10 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+  # Disable database query caching
+  # (this will highlight inefficient queries in development)
+  config.middleware.delete 'ActiveRecord::QueryCache'
+
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
