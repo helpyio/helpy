5b2db1cc08c39b30e68932e0ff19f097c42909e0
Scott Miller <scott.miller.utah@gmail.com>
2016-03-03 17:53:40 -0700

    Suppress email from tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,7 +7,7 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
-  Settings.send_email = true
+  Settings.send_email = false
 end
 
 class ActionController::TestCase
