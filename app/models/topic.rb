4b4788fb5bf613c1eb891c0f6a59f06642258ccf
Ollie Bennett <olliebennett@gmail.com>
2016-03-25 09:13:58 +0000

    Expose individual agent stats for respeonses

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -52,6 +52,7 @@ class Topic < ActiveRecord::Base
   scope :mine, -> (user) { where(assigned_user_id: user) }
   scope :closed, -> { where(current_status: "closed") }
   scope :spam, -> { where(current_status: "spam")}
+  scope :assigned, -> { where.not(assigned_user_id: nil) }
 
   scope :chronologic, -> { order('updated_at DESC') }
   scope :by_popularity, -> { order('points DESC') }
