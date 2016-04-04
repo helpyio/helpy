da86961aa9aa67d4db93e29042eca839ea63433a
Scott Miller <scott.miller.utah@gmail.com>
2016-04-03 22:01:29 -0600

    Convert design namespace to css, add css_overrides

--- config/initializers/default_settings.rb
+++ config/initializers/default_settings.rb
@@ -9,21 +9,22 @@ AppSettings.defaults['settings.product_name'] = Settings.product_name
 AppSettings.defaults['settings.support_phone'] = Settings.support_phone
 AppSettings.defaults['settings.google_analytics_id'] = Settings.google_analytics_id
 
-# Images:
+# Design: (Colors etc.)
+
 AppSettings.defaults['design.favicon'] = Settings.app_favicon
 AppSettings.defaults['design.header_logo'] = Settings.app_mini_logo #Note: the contributer accidentally reversed these in the code
 AppSettings.defaults['design.footer_mini_logo'] = Settings.app_large_logo
-
-# Design: (Colors etc.)
-AppSettings.defaults['design.search_background'] = 'feffe9'
-AppSettings.defaults['design.top_bar'] = '3cceff'
-AppSettings.defaults['design.link_color'] = '004084'
-AppSettings.defaults['design.form_background'] = ''
-AppSettings.defaults['design.still_need_help'] = 'ffdf91'
+AppSettings.defaults['css.search_background'] = 'feffe9'
+AppSettings.defaults['css.top_bar'] = '3cceff'
+AppSettings.defaults['css.link_color'] = '004084'
+AppSettings.defaults['css.form_background'] = 'F0FFF0'
+AppSettings.defaults['css.still_need_help'] = 'ffdf91'
 
 # i18n:
+
 AppSettings.defaults['i18n.default_locale'] = 'en'
 AppSettings.defaults['i18n.available_locales'] = ''.split(',')
 
 # Widget:
-AppSettings.defaults['widget.show_on_support_site'] = true
+
+AppSettings.defaults['widget.show_on_support_site'] = 'true'
