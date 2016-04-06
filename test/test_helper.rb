95859dbb8f0ba1772a6e1c4e391cd1351e5878fb
Scott Miller <scott.miller.utah@gmail.com>
2016-04-05 20:25:52 -0600

    unrequire helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,6 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-require "codeclimate-test-reporter"
 
 CodeClimate::TestReporter.start
 
