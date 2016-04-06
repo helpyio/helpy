16f49d53e09620e47da5763a2f59ad956fbd3c24
Scott Miller <scott.miller.utah@gmail.com>
2016-04-05 20:27:10 -0600

    .

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,8 +2,6 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
-CodeClimate::TestReporter.start
-
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
