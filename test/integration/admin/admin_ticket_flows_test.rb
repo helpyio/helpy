0106e6b6cab8995f543aa9fa68ce1f4ed40b68a2
Scott Miller <scott.miller.utah@gmail.com>
2016-04-07 11:22:33 -0600

    .

--- test/integration/admin/admin_ticket_flows_test.rb
+++ test/integration/admin/admin_ticket_flows_test.rb
@@ -48,7 +48,7 @@ class AdminTicketFlowsTest < ActionDispatch::IntegrationTest
     sleep(2)
     assert page.has_content?("#1- Private topic")
     click_on("#1- Private topic")
-    # ind("a.topic-link").click
+    # find("a.topic-link").click
     sleep(1)
   end
 
@@ -113,91 +113,91 @@ class AdminTicketFlowsTest < ActionDispatch::IntegrationTest
 
   end
 
-#   test "an admin should be able to click on a listed discussion to view it" do
-#     assert current_path == "/admin"
-#
-#     admin_create_discussion("New test message from admin form")
-#
-#     save_screenshot('new-discussion.png', :full => true)
-#     click_on("New")
-#     sleep(1)
-#     #click_on("##{@ticket.id}- New test message from admin form")
-#
-#     within first("tr.topic") do
-#       find(".topic-link").click
-#     end
-#
-#     sleep(1)
-#     assert page.has_content?("Reply to this Topic")
-#
-#   end
-#
-#   test "an admin should be able to click on a listed discussion to reply to it" do
-#
-#     admin_create_discussion("Discussion for a reply")
-#
-#     click_on("New")
-#     sleep(1)
-# #    click_on("##{@ticket.id}- Discussion for a reply")
-#     within first("tr.topic") do
-#       find(".topic-link").click
-#     end
-#
-#     sleep(1)
-#     assert page.has_content?("Reply to this Topic")
-#
-#     # Reply with text
-#     fill_in("post_body", with: "This is a reply, check it out")
-#     sleep(1)
-#     find(".submit-post-reply").click
-#     sleep(1)
-#     assert page.has_content?("Admin User replied...")
-#
-#   end
-#
-#   test "an admin should be able to click on a listed discussion and post an internal note to it" do
-#
-#     admin_create_discussion("Discussion for internal note")
-#
-#     click_on("New")
-#     sleep(1)
-#     within first("tr.topic") do
-#       find(".topic-link").click
-#     end
-#     sleep(1)
-#     assert page.has_content?("Reply to this Topic")
-#
-#     # Reply with internal note
-#     choose("post_kind_note")
-#     fill_in("post_body", with: "This is an internal note")
-#     sleep(1)
-#     find(".submit-post-reply").click
-#     sleep(2)
-#
-#     assert page.has_content?("Admin User posted an internal note...")
-#     assert page.has_content?("This is an internal note")
-#   end
-#
-#   test "an admin should be able to click on a listed discussion and reply with a common reply" do
-#
-#     admin_create_discussion("Discussion for common reply")
-#
-#     click_on("New")
-#     sleep(1)
-#     within first("tr.topic") do
-#       find(".topic-link").click
-#     end
-#
-#     sleep(1)
-#     assert page.has_content?("Reply to this Topic")
-#
-#     #Reply with common reply
-#     select('Article 1', from: 'post_reply_id')
-#     find(".submit-post-reply").click
-#     sleep(1)
-#
-#     assert page.has_content?("article1 text")
-#   end
+  test "an admin should be able to click on a listed discussion to view it" do
+    assert current_path == "/admin"
+
+    admin_create_discussion("New test message from admin form")
+
+    click_on("New")
+    sleep(1)
+    #click_on("##{@topic.id}- New test message from admin form")
+
+    within first("tr.topic") do
+      find(".topic-link").click
+    end
+
+    sleep(1)
+    assert page.has_content?("Reply to this Topic")
+
+  end
+
+  test "an admin should be able to click on a listed discussion to reply to it" do
+
+    admin_create_discussion("Discussion for a reply")
+
+    click_on("New")
+    sleep(1)
+#    click_on("##{@topic.id}- Discussion for a reply")
+    within("tr#topic-#{@topic.id}") do
+      find(".topic-link").click
+    end
+
+    sleep(1)
+    assert page.has_content?("Reply to this Topic")
+
+    # Reply with text
+    fill_in("post_body", with: "This is a reply, check it out")
+    sleep(1)
+    find(".submit-post-reply").click
+    sleep(1)
+    assert page.has_content?("Admin User replied...")
+
+  end
+
+  test "an admin should be able to click on a listed discussion and post an internal note to it" do
+
+    admin_create_discussion("Discussion for internal note")
+
+    click_on("New")
+    sleep(1)
+    within("tr#topic-#{@topic.id}") do
+      find(".topic-link").click
+    end
+
+    sleep(1)
+    assert page.has_content?("Reply to this Topic")
+
+    # Reply with internal note
+    choose("post_kind_note")
+    fill_in("post_body", with: "This is an internal note")
+    sleep(1)
+    find(".submit-post-reply").click
+    sleep(2)
+
+    assert page.has_content?("Admin User posted an internal note...")
+    assert page.has_content?("This is an internal note")
+  end
+
+  test "an admin should be able to click on a listed discussion and reply with a common reply" do
+
+    admin_create_discussion("Discussion for common reply")
+
+    click_on("New")
+    sleep(1)
+    within("tr#topic-#{@topic.id}") do
+      find(".topic-link").click
+    end
+
+    sleep(1)
+    assert page.has_content?("Reply to this Topic")
+
+    #Reply with common reply
+    select('Article 1', from: 'post_reply_id')
+    find(".submit-post-reply").click
+    sleep(1)
+
+    assert page.has_content?("article1 text")
+  end
 
 #   test "an admin should be able to edit deactivate and turn a post into content" do
 #
@@ -205,11 +205,11 @@ class AdminTicketFlowsTest < ActionDispatch::IntegrationTest
 #
 #     click_on("New")
 #     sleep(2)
-#     within("tr#topic-#{@ticket.id}") do
+#     within("tr#topic-#{@topic.id}") do
 #       find(".topic-link").click
 #     end
 #
-# #    click_on("##{@ticket.id}- Discussion for post")
+# #    click_on("##{@topic.id}- Discussion for post")
 #     sleep(2)
 #     assert page.has_content?("Reply to this Topic")
 #
