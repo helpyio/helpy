9a74bc2f3c897977cd6efa55e895f60b8d1c893e
Scott Miller <scott.miller.utah@gmail.com>
2016-04-09 13:21:15 -0600

    Test removing default settings from test harness

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -23,6 +23,7 @@ def default_settings
   Rails.cache.clear
 
   # Loads default settings
+=begin
   AppSettings["settings.parent_site"] = Settings.parent_site
   AppSettings["settings.parent_company"] = Settings.parent_company
   AppSettings["settings.site_url"] = Settings.site_url
@@ -42,4 +43,5 @@ def default_settings
   AppSettings["i18n.default_locale"] = "en"
   AppSettings["i18n.available_locales"] = ["en", "fr", "de", "et"]
   AppSettings["widget.show_on_support_site"] = "true"
+=end
 end
