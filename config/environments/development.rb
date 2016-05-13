7aed055c7e1f7f98a8720897c4b53fddc97209e8
Scott Miller <scott.miller.utah@gmail.com>
2016-05-13 10:39:28 -0600

    Adjust naming to get language to show up in settings view/picker

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,7 +47,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.i18n.available_locales = [:en, :es, :de, :fr, :et, :ca, :ru, :ja, 'zh-cn', 'zh-tw', 'pt', :nl, 'tr']
+  config.i18n.available_locales = [:en, :es, :de, :fr, :et, :ca, :ru, :ja, 'zh-cn', 'zh-tw', 'pt', :nl, 'tr', 'pt-br']
   config.i18n.default_locale = :en
   config.i18n.fallbacks = true
 
