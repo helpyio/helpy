4137175f92e31b5701e9903761c9128dba3892d8
Scott Miller <scott.miller.utah@gmail.com>
2016-06-03 10:29:26 -0600

    Improve flat theme, fix bugs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,3 @@
 class SessionsController < Devise::SessionsController
   theme :theme_chosen
-  layout :which_layout, only: 'new'
-
-  def new
-    super
-  end
-
-  protected
-
-  def which_layout
-    if AppSettings['theme.active'] == 'helpy'
-      'devise'
-    else
-      AppSettings['theme.active']
-    end
-  end
-
 end
