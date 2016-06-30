473a3886ac5e60dc5b4d29159eac9b9815cb5eb8
Joe Connor <joe.connor@factorymedia.com>
2016-06-30 22:51:06 +0800

    Revert Doorkeeper Helpers to keep PR atomic

--- app/controllers/api/v1/docs.rb
+++ app/controllers/api/v1/docs.rb
@@ -3,15 +3,13 @@ require 'doorkeeper/grape/helpers'
 module API
   module V1
     class Docs < Grape::API
-      helpers Doorkeeper::Grape::Helpers
+      #helpers Doorkeeper::Grape::Helpers
       #
       # before do
       #   doorkeeper_authorize!
       # end
-      
       before do
         authenticate!
-        restrict_to_role %w(admin agent)
       end
 
       include API::V1::Defaults
