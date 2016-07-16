26e41636b98e50d195c30d86df5246a4e8629d8b
Scott Miller <scott.miller.utah@gmail.com>
2016-07-16 15:03:17 -0600

    Remove nil from search_controller too

--- app/controllers/admin/search_controller.rb
+++ app/controllers/admin/search_controller.rb
@@ -20,19 +20,19 @@ class Admin::SearchController < Admin::BaseController
     if users.size == 0 # not a user search, so look for topics
       @topics = Topic.admin_search(params[:q]).page params[:page]
       template = 'admin/topics/index'
-      tracker("Admin Search", "Topic Search", params[:q], nil)
+      tracker("Admin Search", "Topic Search", params[:q])
     elsif users.size == 1
       @user = users.first
       @topics = Topic.admin_search(params[:q]).page params[:page]
       @topic = Topic.where(user_id: @user.id).first unless @user.nil?
       template = 'admin/topics/index'
 
-      tracker("Admin Search", "User Search", params[:q], nil)
-      tracker("Agent: #{current_user.name}", "Viewed User Profile", @user.name, nil)
+      tracker("Admin Search", "User Search", params[:q])
+      tracker("Agent: #{current_user.name}", "Viewed User Profile", @user.name)
     else
       @users = users.page params[:page]
       template = 'admin/users/users'
-      tracker("Admin Search", "User Search", params[:q], nil)
+      tracker("Admin Search", "User Search", params[:q])
     end
 
     render template
