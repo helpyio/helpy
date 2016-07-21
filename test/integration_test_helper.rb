b4a3b1cc9cdeacb7b380cb3de0783056ef4dcfdd
Joshua Clayton <joshua.clayton@gmail.com>
2016-07-21 05:54:31 -0400

    Remove sign_in_by_modal
    Usage of sign_in_by_modal was removed in
    9b8cf6147fea14e2fea7d403969f725895187431.

--- test/integration_test_helper.rb
+++ test/integration_test_helper.rb
@@ -15,17 +15,6 @@ def sign_in(email='scott.miller@test.com')
   end
 end
 
-def sign_in_by_modal(email='scott.miller@test.com')
-  within("div#above-header") do
-    click_on "Sign in", wait: 30
-  end
-  within('div#login-modal') do
-    fill_in("user[email]", with: email)
-    fill_in("user[password]", with: '12345678')
-    click_on('Sign in')
-  end
-end
-
 def sign_out
   logout(:user)
 end
