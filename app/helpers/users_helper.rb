d0187b5763771bc79c0c9c97be214c62007e05fd
Joshua Clayton <joshua.clayton@gmail.com>
2016-07-21 05:49:02 -0400

    Remove UsersHelper#user_avatar
    This method was replaced by UsersHelper#avatar_image and removed from
    views in various commits, including
    e9bd26e673695a52441fedc905e63a0f9c6beca8,
    d301a462d74079d8e1d3acefbe7f26ab320e2b02, and
    e16ba1a0e02cb17b2ec9a7b2818e42d1676eac95

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -46,16 +46,6 @@
 
 module UsersHelper
 
-  def user_avatar(user, size=40)
-    if user.thumbnail == "" && user.avatar.nil?
-      user.gravatar_url(:size => 60)
-    elsif user.avatar.nil? == false
-      "https://res.cloudinary.com/helpy-io/image/upload/c_thumb,w_#{size},h_#{size}/#{user.avatar.path}"
-    else
-      user.thumbnail
-    end
-  end
-
   def avatar_image(user, size=40)
 
     if user.avatar.nil? == false
