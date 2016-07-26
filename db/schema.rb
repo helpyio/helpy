2aac19588e35bc0b4c24a7cde30f03bb330e5bb7
Scott Miller <scott.miller.utah@gmail.com>
2016-07-26 06:56:46 -0600

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160627171450) do
+ActiveRecord::Schema.define(version: 20160709052116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -293,6 +293,7 @@ ActiveRecord::Schema.define(version: 20160627171450) do
     t.integer  "invited_by_id"
     t.string   "invited_by_type"
     t.integer  "invitations_count",      default: 0
+    t.text     "invitation_message"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
