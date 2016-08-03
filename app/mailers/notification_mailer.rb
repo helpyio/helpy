2415882dca342782b7ffcdd9865144dc1e809579
Scott Miller <scott.miller.utah@gmail.com>
2016-08-03 11:35:31 -0600

    Update mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -3,16 +3,19 @@ class NotificationMailer < ActionMailer::Base
   def new_private(topic)
     @topic = topic
     mail(
-      to: "admin@helpy.io", # User.with_settings_for('notify_on_private').collect { |u| u.email },
-      from: %("#{AppSettings['settings.site_name']}" <#{AppSettings['email.admin_email']}>),
+      to: User.with_settings_for('notify_on_private').collect { |u| u.email },
+      # cc:
+      from: "noreply@#{AppSettings['settings.site_url']}",
       subject: "[#{AppSettings['settings.site_name']}] ##{topic.id}-#{topic.name}"
       )
   end
 
   def new_public(topic)
+    @topic = topic
     mail(
-      to: "admin@helpy.io", # User.with_settings_for('notify_on_public').collect { |u| u.email },
-      from: %("#{AppSettings['settings.site_name']}" <#{AppSettings['email.admin_email']}>),
+      to: User.with_settings_for('notify_on_private').collect { |u| u.email },
+      # cc: User.with_settings_for('notify_on_public').collect { |u| u.email },
+      from: "noreply@#{AppSettings['settings.site_url']}",
       subject: "[#{AppSettings['settings.site_name']}] ##{topic.id}-#{topic.name}"
       )
   end
@@ -20,8 +23,9 @@ class NotificationMailer < ActionMailer::Base
   def new_reply(topic)
     @topic = topic
     mail(
-      to: "admin@helpy.io", # User.with_settings_for('notify_on_reply').collect { |u| u.email },
-      from: %("#{AppSettings['settings.site_name']}" <#{AppSettings['email.admin_email']}>),
+      to: User.with_settings_for('notify_on_reply').collect { |u| u.email },
+      # cc: User.with_settings_for('notify_on_reply').collect { |u| u.email },
+      from: "noreply@#{AppSettings['settings.site_url']}",
       subject: "[#{AppSettings['settings.site_name']}] ##{topic.id}-#{topic.name}"
       )
   end
