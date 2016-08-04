37988dee18670b0f0e11112aa5d694e565716d55
Scott Miller <scott.miller.utah@gmail.com>
2016-08-04 10:49:08 -0600

    Use correct from address for Devise, custom subject for reset pw

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -1,11 +1,17 @@
 class DeviseMailer < Devise::Mailer
 
+  helper :application # gives access to all helpers defined within `application_helper`.
+  include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
+  default template_path: 'devise/mailer' # to make sure that your mailer uses the devise
+
   def confirmation_instructions(record, token, opts={})
     # code to be added here later
   end
 
   def reset_password_instructions(record, token, opts={})
-    # code to be added here later
+    opts[:from] = "noreply@#{AppSettings['settings.site_url']}"
+    opts[:subject] = "[#{AppSettings['settings.site_name']}] #{t('devise.mailer.reset_password_instructions.subject')}"
+    super
   end
 
   def unlock_instructions(record, token, opts={})
