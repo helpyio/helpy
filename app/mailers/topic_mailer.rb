a2bb2a4b27b57bcfc8c8dd579dc9bb4979be1119
Scott Miller <scott.miller.utah@gmail.com>
2016-08-06 10:38:36 -0600

    RTL email templates

--- app/mailers/topic_mailer.rb
+++ app/mailers/topic_mailer.rb
@@ -1,7 +1,9 @@
 class TopicMailer < ActionMailer::Base
+  add_template_helper(ApplicationHelper)
 
   def new_ticket(topic)
     @topic = topic
+    @locale = "fa" || 'en'
     email_with_name = %("#{topic.user.name}" <#{topic.user.email}>)
     mail(
       to: email_with_name,
