f2f4f8a8faa316300adb15e5f03f82e72376261f
Scott Miller <scott.miller.utah@gmail.com>
2016-09-02 14:54:15 -0600

    Fixed tests

--- test/controllers/errors_controller_test.rb
+++ test/controllers/errors_controller_test.rb
@@ -3,12 +3,12 @@ require 'test_helper'
 class ErrorsControllerTest < ActionController::TestCase
   test "should get not_found" do
     get :not_found
-    assert_response :success
+    assert_response :missing
   end
 
   test "should get internal_server_error" do
     get :internal_server_error
-    assert_response :success
+    assert_response :error
   end
 
 end
