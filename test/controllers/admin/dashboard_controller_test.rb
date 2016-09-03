94a97fd19f966854ff31a9be383c04529ff752d8
Christian Gregg <c_arlt@hotmail.com>
2016-09-03 16:42:55 +0100

    Add authorisation tests

--- test/controllers/admin/dashboard_controller_test.rb
+++ test/controllers/admin/dashboard_controller_test.rb
@@ -4,4 +4,34 @@ class Admin::DashboardControllerTest < ActionController::TestCase
   setup do
     set_default_settings
   end
+
+  # GET #index
+  %w(agent admin).each do |user|
+    test "a(n) #{user} should redirect" do
+      sign_in users(user.to_sym)
+      get :index, locale: :en
+      assert_redirected_to admin_topics_path
+    end
+  end
+
+  test 'an editor should redirect' do
+    sign_in users(:editor)
+    get :index, locale: :en
+    assert_redirected_to admin_categories_path
+  end
+
+  # GET #stats
+  test 'an admin should get stats' do
+    sign_in users(:admin)
+    get :stats, locale: :en
+    assert_response :success
+  end
+
+  %w(agent editor user).each do |unauthorized_user|
+    test "a(n) #{unauthorized_user} should not get stats" do
+      sign_in users(unauthorized_user.to_sym)
+      get :stats, locale: :en
+      assert_redirected_to root_path
+    end
+  end
 end
