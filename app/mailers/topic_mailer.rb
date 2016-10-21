314d863fdc3b60b3095edea56fc2dcbd3cb0867c
Scott Miller <scott.miller.utah@gmail.com>
2016-10-21 15:00:10 -0600

    Include attachments in email

--- app/mailers/topic_mailer.rb
+++ app/mailers/topic_mailer.rb
@@ -4,6 +4,9 @@ class TopicMailer < ActionMailer::Base
   def new_ticket(topic)
     @topic = topic
     email_with_name = %("#{topic.user.name}" <#{topic.user.email}>)
+    @topic.posts.last.attachments.each do |att|
+      attachments[att.file.filename] = File.read(att.file.file)
+    end
     mail(
       to: email_with_name,
       from: %("#{AppSettings['settings.site_name']}" <#{AppSettings['email.admin_email']}>),
