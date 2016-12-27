3bb00d05a8e74d768bf6846bebad3bbe2c8ab6ee
Scott Miller <scott.miller.utah@gmail.com>
2016-12-27 12:56:35 -0700

    Move search bar, add search by date and team

--- app/controllers/admin/search_controller.rb
+++ app/controllers/admin/search_controller.rb
@@ -4,6 +4,7 @@ class Admin::SearchController < Admin::BaseController
   before_action :fetch_counts
   before_action :remote_search
   before_action :get_all_teams
+  before_action :search_date_from_params
 
   respond_to :html, :js
 
@@ -20,7 +21,7 @@ class Admin::SearchController < Admin::BaseController
 
     if users.size == 0 # not a user search, so look for topics
       search_topics
-      template = 'admin/topics/index'
+      template = 'admin/search/search'
       tracker("Admin Search", "Topic Search", params[:q])
     elsif users.size == 1
       @user = users.first
@@ -41,11 +42,27 @@ class Admin::SearchController < Admin::BaseController
   protected
 
   def search_topics
+    topics_to_search = Topic.where('created_at >= ?', @start_date).where('created_at <= ?', @end_date)
     if current_user.is_restricted? && teams?
-      @topics = Topic.admin_search(params[:q]).tagged_with(current_user.team_list, :any => true).page params[:page]
+      @topics = topics_to_search.admin_search(params[:q]).tagged_with(current_user.team_list, :any => true).page params[:page]
     else
-      @topics = Topic.admin_search(params[:q]).page params[:page]
+      @topics = topics_to_search.admin_search(params[:q]).page params[:page]
     end
   end
+
+  def search_date_from_params
+    if params[:start_date].present?
+      @start_date = params[:start_date].to_datetime
+    else
+      @start_date = Time.zone.today-1.month
+    end
+
+    if params[:end_date].present?
+      @end_date = params[:end_date].to_datetime
+    else
+      @end_date = Time.zone.today.at_end_of_day
+    end
+  end
+
 
 end
