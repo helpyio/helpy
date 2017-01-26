0e242c6b78f9c49a53078bb8398e726c8fff9b7a
Luiz Cezer <lccezinha@gmail.com>
2017-01-25 22:24:50 -0200

    Add new field extra_info to User model
    This field is a jsonb and it will be used to store some
    extra infos about user.
    
    To see more about jsonb read this:
    
    http://nandovieira.com/using-postgresql-and-jsonb-with-ruby-on-rails

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161017170305) do
+ActiveRecord::Schema.define(version: 20170126000313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -267,6 +267,7 @@ ActiveRecord::Schema.define(version: 20161017170305) do
     t.text     "invitation_message"
     t.string   "time_zone",              default: "UTC"
     t.string   "profile_image"
+    t.jsonb    "extra_info"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
