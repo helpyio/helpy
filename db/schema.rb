17fdac26b5d93e5f87e93939b9b7a984e53b3ed0
Henrique Aparecido Lavezzo <henlavezzo@gmail.com>
2017-02-03 00:25:45 -0200

    Create new internal knowledgebase for agent teams.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170120154323) do
+ActiveRecord::Schema.define(version: 20170127010804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,29 +44,6 @@ ActiveRecord::Schema.define(version: 20170120154323) do
 
   add_index "attachinary_files", ["attachinariable_type", "attachinariable_id", "scope"], name: "by_scoped_parent", using: :btree
 
-  create_table "audits", force: :cascade do |t|
-    t.integer  "auditable_id"
-    t.string   "auditable_type"
-    t.integer  "associated_id"
-    t.string   "associated_type"
-    t.integer  "user_id"
-    t.string   "user_type"
-    t.string   "username"
-    t.string   "action"
-    t.text     "audited_changes"
-    t.integer  "version",         default: 0
-    t.string   "comment"
-    t.string   "remote_address"
-    t.string   "request_uuid"
-    t.datetime "created_at"
-  end
-
-  add_index "audits", ["associated_id", "associated_type"], name: "associated_index", using: :btree
-  add_index "audits", ["auditable_id", "auditable_type"], name: "auditable_index", using: :btree
-  add_index "audits", ["created_at"], name: "index_audits_on_created_at", using: :btree
-  add_index "audits", ["request_uuid"], name: "index_audits_on_request_uuid", using: :btree
-  add_index "audits", ["user_id", "user_type"], name: "user_index", using: :btree
-
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.string   "icon"
@@ -80,6 +57,7 @@ ActiveRecord::Schema.define(version: 20170120154323) do
     t.string   "section"
     t.datetime "created_at",                       null: false
     t.datetime "updated_at",                       null: false
+    t.boolean  "public",           default: true
   end
 
   create_table "category_translations", force: :cascade do |t|
