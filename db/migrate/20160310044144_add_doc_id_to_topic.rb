fad4f60eeb05bbd9bbf9742ce3e9b526de948b1b
Scott Miller <scott.miller.utah@gmail.com>
2017-03-06 11:56:04 -0700

    Remove old data update

--- db/migrate/20160310044144_add_doc_id_to_topic.rb
+++ db/migrate/20160310044144_add_doc_id_to_topic.rb
@@ -4,23 +4,12 @@ class AddDocIdToTopic < ActiveRecord::Migration
     add_column :topics, :doc_id, :integer, default: 0
     add_column :docs, :topics_count, :integer, default: 0
     add_column :docs, :allow_comments, :boolean, default: true
-
-    if Forum.where(name: 'Doc comments').first.nil?
-      f = Forum.create!(
-        name: 'Doc comments',
-        description: 'Contains comments to docs',
-        private: true
-        )
-    end
   end
 
   def down
     remove_column :topics, :doc_id
     remove_column :docs, :topics_count
     remove_column :docs, :allow_comments
-
-    @forum = Forum.where(name: 'Doc comments').first
-    @forum.delete unless @forum.nil?
   end
 
 end
