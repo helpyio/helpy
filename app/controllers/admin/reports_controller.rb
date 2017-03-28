9232660fb27e4f1793c6fe99079d26bfbf8fc5dd
Scott Miller <scott.miller.utah@gmail.com>
2017-03-28 17:58:10 -0600

    Some refactoring, styling

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -1,32 +1,14 @@
 class Admin::ReportsController < Admin::BaseController
 
   include StatsHelper
-  skip_before_action :verify_agent
-  before_action :date_from_params, only: [:index, :team]
-  before_action :verify_admin, only: [:index, :team]
+  before_action :verify_admin
   before_action :get_all_teams
 
+  before_action :date_from_params, only: [:index, :team]
+  before_action :scope_data, only: [:index, :team]
+  before_action :set_interval, only: [:index, :team]
 
   def index
-    @interval = case params[:label]
-                  when 'today'
-                    t('today')
-                  when 'yesterday'
-                    t('yesterday')
-                  when 'this_week'
-                    t('this_week')
-                  when 'last_week'
-                    t('last_week')
-                  when 'this_month'
-                    t('this_month')
-                  when 'last_month'
-                    t('last_month')
-                  when 'interval'
-                    "Between #{@start_date.to_date} and #{@end_date.to_date}"
-                  else
-                    t('this_week')
-                end
-
     number_of_days = @end_date.to_date - @start_date.to_date
     if number_of_days == 1
       get_hourly_stats
@@ -36,26 +18,6 @@ class Admin::ReportsController < Admin::BaseController
   end
 
   def team
-
-    @interval = case params[:label]
-                  when 'today'
-                    t('today')
-                  when 'yesterday'
-                    t('yesterday')
-                  when 'this_week'
-                    t('this_week')
-                  when 'last_week'
-                    t('last_week')
-                  when 'this_month'
-                    t('this_month')
-                  when 'last_month'
-                    t('last_month')
-                  when 'interval'
-                    "Between #{@start_date.to_date} and #{@end_date.to_date}"
-                  else
-                    t('this_week')
-                end
-
     @topics = Topic.undeleted.where('topics.created_at >= ? AND topics.created_at <= ?', @start_date, @end_date)
     @topic_count = @topics.count
 
@@ -79,37 +41,57 @@ class Admin::ReportsController < Admin::BaseController
 
   private
 
-  def get_daily_stats
-    get_stats_from_topics if @scoped_stats.blank?
+  def set_interval
+    @interval = case params[:label]
+                  when 'today'
+                    t('today')
+                  when 'yesterday'
+                    t('yesterday')
+                  when 'this_week'
+                    t('this_week')
+                  when 'last_week'
+                    t('last_week')
+                  when 'this_month'
+                    t('this_month')
+                  when 'last_month'
+                    t('last_month')
+                  when 'interval'
+                    "Between #{@start_date.to_date} and #{@end_date.to_date}"
+                  else
+                    t('this_week')
+                end
+
   end
 
-  def get_stats_from_topics
+  def scope_data
     @scoped_stats = Topic.where('topics.created_at >= ? AND topics.created_at <= ?', @start_date, @end_date)
+    @scoped_posts = Post.where('created_at >= ? AND created_at <= ?', @start_date, @end_date)
+  end
+
+  def get_daily_stats
     unless @scoped_stats.nil?
       @tickets = @scoped_stats.group_by_day(:created_at).count
-      @closed = @scoped_stats.where(current_status: 'closed').group_by_day(:created_at).count
-      @responded = {} #@scoped_stats.group_by_day(:created_at).sum(:responded)
-
-      @total_tickets = @scoped_stats.count
-      @total_replied = @scoped_stats.where('posts_count > 0').count
-      @total_closed = @scoped_stats.where(current_status: 'closed').count
+      @closed = @scoped_stats.where(current_status: 'closed').group_by_day(:updated_at).count
+      @actions = @scoped_posts.group_by_day(:created_at).count
     end
+    get_total_stats
   end
 
   def get_hourly_stats
-    @scoped_stats = Topic.where('topics.created_at >= ? AND topics.created_at <= ?', @start_date, @end_date)
-
     unless @scoped_stats.nil?
       @tickets = @scoped_stats.group_by_hour_of_day(:created_at).count
       @closed = @scoped_stats.where(current_status: 'closed').group_by_hour_of_day(:created_at).count
-      @responded = {} #@scoped_stats.group_by_day(:created_at).sum(:responded)
-
-      @total_tickets = @scoped_stats.count
-      @total_replied = @scoped_stats.where('posts_count > 0').count
-      @total_closed = @scoped_stats.where(current_status: 'closed').count
+      @actions = @scoped_posts.group_by_day(:created_at).count
     end
+    get_total_stats
   end
 
+  def get_total_stats
+    @total_tickets = @scoped_stats.count
+    @total_replies = @scoped_posts.where(kind: 'reply').count
+    @total_closed = @scoped_stats.where(current_status: 'closed').count
+    @total_activities = @scoped_posts.count
+  end
 
 
 end
