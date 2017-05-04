ae1e542982ffce2cbed145e8ed2cdd8d9c72d001
Scott Miller <scott.miller.utah@gmail.com>
2017-05-04 11:45:24 -0600

    Fix broken option to unassign a ticket from a group

--- test/controllers/admin/topics_controller_test.rb
+++ test/controllers/admin/topics_controller_test.rb
@@ -137,6 +137,21 @@ class Admin::TopicsControllerTest < ActionController::TestCase
       assert_response :success
     end
 
+    ### test assign_team and unassign_team
+
+    test "an #{admin} should be able to assign_team of a topic" do
+      sign_in users(admin.to_sym)
+      xhr :get, :assign_team, { topic_ids: [1], team: "test"}
+      assert_equal ["test"], Topic.find(1).team_list
+    end
+
+    test "an #{admin} should be able to unassign_team of a topic" do
+      Topic.find(1).team_list = "test"
+      sign_in users(admin.to_sym)
+      xhr :get, :unassign_team, { topic_ids: [1]}
+      assert_equal [], Topic.find(1).team_list
+    end
+
 
     ### tests of changing status
 
@@ -261,6 +276,11 @@ class Admin::TopicsControllerTest < ActionController::TestCase
         end
       end
     end
+
+    # Tests of team group assignment/unassignment
+
+
+
 
     # NOTE: THIS BEHAVIOR WAS REVERSED BASED ON USER FEEDBACK THAT IT WAS HARD TO
     # FIND DISCUSSIONS AFTER THEY WERE VIEWED, LEFT TEST JUST IN CASE
