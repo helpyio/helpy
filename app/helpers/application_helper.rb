002f395477fd98e0284a52a9b097f35931de38d2
scott <scott.miller.utah@gmail.com>
2017-06-09 10:52:37 -0600

    Allow html and emoji from admin create form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -135,7 +135,7 @@ module ApplicationHelper
   # https://github.com/github/gemoji
   def emojify(content)
     content.to_str.gsub(/:([\w+-]+):/) do |match|
-      if emoji = Emoji.find_by_alias($1)
+      if emoji == Emoji.find_by_alias($1)
         %(<img alt="#$1" src="#{image_path("emoji/#{emoji.image_filename}")}" style="vertical-align:middle" width="20" height="20" />)
       else
         match
