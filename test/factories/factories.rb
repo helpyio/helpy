3343ce0afded7999b283733ab6b2bbcf9952ed2f
TROUVERIE Joachim <jtrouverie@itrust.fr>
2017-12-18 09:18:33 +0100

    Fix code duplication

--- test/factories/factories.rb
+++ test/factories/factories.rb
@@ -90,6 +90,13 @@ FactoryGirl.define do
     end
   end
 
+  factory :email_from_unknown_via_griddler, class: OpenStruct do
+    to [{ full: 'to_user@email.com', email: 'to_user@email.com', token: 'to_user', host: 'email.com', name: nil }]
+    from({ token: 'from_user', host: 'email.com', email: 'from_email@email.com', full: 'From User <from_user@email.com>', name: 'From User' })
+    subject 'email subject'
+    body 'Hello!'
+  end
+
   factory :email_from_known, class: OpenStruct do
     to [{ full: 'to_user@email.com', email: 'to_user@email.com', token: 'to_user', host: 'email.com', name: nil }]
     from({ token: 'scott.miller', host: 'test.com', email: 'scott.miller@test.com', full: 'Scott Miller <scott.miller@test.com>', name: 'Scott Miller' })
