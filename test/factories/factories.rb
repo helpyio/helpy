d78d034654d1a3433e425e4ac8463aba419076e1
Christian Gregg <c_arlt@hotmail.com>
2018-01-23 19:19:28 +0100

    Copy CC emails to ticket

--- test/factories/factories.rb
+++ test/factories/factories.rb
@@ -106,6 +106,15 @@ FactoryGirl.define do
     body 'Hello!'
   end
 
+  factory :email_with_cc, class: OpenStruct do
+    to [{ full: 'to_user@email.com', email: 'to_user@email.com', token: 'to_user', host: 'email.com', name: nil }]
+    from({ token: 'scott.miller', host: 'test.com', email: 'scott.miller@test.com', full: 'Scott Miller <scott.miller@test.com>', name: 'Scott Miller' })
+    cc([{ token: 'from_user', host: 'email.com', email: 'from_email@email.com', full: 'From User <from_user@email.com>', name: 'From User' }])
+    subject 'email subject'
+    header {}
+    body 'Hello!'
+  end
+
   factory :reply_to_closed_ticket, class: OpenStruct do
     to [{ full: 'to_user@email.com', email: 'to_user@email.com', token: 'to_user', host: 'email.com', name: nil }]
     from({ token: 'scott.miller', host: 'test.com', email: 'scott.miller@test.com', full: 'Scott Miller <scott.miller@test.com>', name: 'Scott Miller' })
