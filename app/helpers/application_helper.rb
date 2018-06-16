3a0d5a0aa8f0b18a46baedbcbb1c4bac86755775
scott <scott.miller.utah@gmail.com>
2018-06-16 14:33:14 -0600

    Use summernote helper to load correct locale

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -132,4 +132,9 @@ module ApplicationHelper
     screenshot.format == "pdf" ? "#{screenshot.public_id}.png" : screenshot.path
   end
 
+  def summernote_lang_js
+    return if I18n.locale == 'en'
+    "<script src=\"/assets/summernote/lang/summernote-#{I18n.locale.downcase}-#{I18n.locale.upcase}.js\"></script>".html_safe
+  end
+
 end
