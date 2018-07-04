0b0f799a016618a4b20048cdf2b1fa74bcd5e44e
Trevor Merritt <trevor.merritt@alfcurtis.com>
2018-07-04 11:48:26 -0400

    ENHANCEMENT: Adds 'topics/:id/posts' to pull posts for a specific ticket/topic

--- app/controllers/api/v1/topics.rb
+++ app/controllers/api/v1/topics.rb
@@ -11,11 +11,21 @@ module API
       include Grape::Kaminari
 
       # throttle max: 200, per: 1.minute
-
       # PRIVATE TICKET ENDPOINTS
       resource :tickets, desc: "Create and Manage private discussions" do
 
         paginate per_page: 20
+
+        # List Posts for Ticket
+        desc "Get posts for a specific ticket"
+        params do
+          requires :id, type: Integer, desc: "Ticket ID"
+        end
+        get ":id/posts", root: :topics do
+          posts = Post.where(topic_id: permitted_params[:id])
+          present paginate(posts), with: Entity::Post
+        end
+
 
         desc "List all PRIVATE tickets", {
           entity: Entity::Topic,
