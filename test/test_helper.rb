7be9afd88551bd4d534c1bce78effc0e9028a727
HoangDinh <dhoangk07@gmail.com>
2018-09-23 11:25:07 +0700

    Fix Chrome can not start
    https://github.com/travis-ci/travis-ci/issues/7150

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -23,7 +23,7 @@ class ActiveSupport::TestCase
 end
 
 Capybara.register_driver :chrome do |app|
-  options = Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox headless disable-gpu])
+  options = Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox headless disable-gpu --disable-dev-shm-usage])
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 
