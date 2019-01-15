53221fed08f0718ab30f93a99c72971affde682b
scott <scott.miller.utah@gmail.com>
2019-01-15 10:33:26 -0700

    Update env variables

--- config/initializers/fog-aws.rb
+++ config/initializers/fog-aws.rb
@@ -11,16 +11,18 @@ CarrierWave.configure do |config|
     config.fog_directory =    'testbucket'
 
   else
-    config.fog_credentials = {
-      provider:              'AWS',
-      aws_access_key_id:     ENV['S3_KEY'],
-      aws_secret_access_key: ENV['S3_SECRET'],
-      region:                ENV['S3_REGION'],
-      # host:                's3.example.com',
-      # endpoint:            'https://s3.example.com:8080'
-    }
-    config.fog_directory  = ENV['S3_BUCKET_NAME']
-    config.fog_public     = true
-    config.fog_attributes = { cache_control: "public, max-age=#{365.day.to_i}" }
+    if ENV['REMOTE_STORAGE'] == "true"
+      config.fog_credentials = {
+        provider:              'AWS',
+        aws_access_key_id:     ENV['S3_KEY'],
+        aws_secret_access_key: ENV['S3_SECRET'],
+        region:                ENV['S3_REGION'],
+        # host:                's3.example.com',
+        endpoint:            ENV['S3_ENDPOINT']
+      }
+      config.fog_directory  = ENV['S3_BUCKET_NAME']
+      config.fog_public     = true
+      config.fog_attributes = { cache_control: "public, max-age=#{365.day.to_i}" }
+    end
   end
 end
