d1cacc11ba410c40ddb49f9b614be59f4ec88636
scott <scott.miller.utah@gmail.com>
2019-03-01 14:11:48 -0700

    Extract onboard from logged in to logged out experience

--- vendor/helpy_onboarding/app/controllers/decorators/application_controller_decorator.rb
+++ vendor/helpy_onboarding/app/controllers/decorators/application_controller_decorator.rb
@@ -5,8 +5,7 @@ ApplicationController.class_eval do
   # Here we are just checking if the onboarding should be shown, based on the
   # current admin username.
   def show_onboarding?
-    # AppSettings['onboarding.complete'] == '0'
-    User.first.email == 'admin@test.com' && User.first.email == 'admin@test.com' && User.first.is_admin?
+    AppSettings['onboarding.complete'] == '0'
   end
 
 end
