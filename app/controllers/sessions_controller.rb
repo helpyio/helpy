93e8e36d9c1b1fb8861203ba5f432ec0aac8c6a9
Sixto Martin <pitbulk@gmail.com>
2019-03-29 14:17:23 +0100

    Support Single Logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,23 @@
 class SessionsController < Devise::SessionsController
+
   theme :theme_chosen
+
+  def destroy
+    # Preserve the saml_uid in the session
+    if session.key?('saml_uid') && session["saml_uid"]
+        saml_uid = session["saml_uid"]
+        super do
+            session["saml_uid"] = saml_uid
+        end
+    end
+  end
+
+  def after_sign_out_path_for(_)
+    if session.key?('saml_uid') && session["saml_uid"]
+      session.delete("saml_uid")
+      user_saml_omniauth_authorize_path + "/spslo"
+    else
+      super
+    end
+  end
 end
