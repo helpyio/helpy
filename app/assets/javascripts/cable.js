46b92e51e790b76f16280eb272165fe5acb25a08
scott <scott.miller.utah@gmail.com>
2019-05-28 11:34:53 -0600

    Ignore cable.js jshint linter failure

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -1,3 +1,5 @@
+// jshint ignore: start
+
 // Action Cable provides the framework to deal with WebSockets in Rails.
 // You can generate new channels where WebSocket features live using the rails generate channel command.
 //
