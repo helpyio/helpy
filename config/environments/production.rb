e4e962900689a8ef099440a9236536048ea38c0d
Trevor Merritt <trevor.merritt@alfcurtis.com>
2019-05-30 10:56:17 -0400

    Attachments were not being served

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,8 +22,8 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
+  config.serve_static_files = true
+  
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
