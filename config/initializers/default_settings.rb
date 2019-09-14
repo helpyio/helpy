2ea7b10dea17dd788b303166bda27ac70282fc79
scott <scott.miller.utah@gmail.com>
2019-09-13 19:57:01 -0600

    Use setting for default reply type

--- config/initializers/default_settings.rb
+++ config/initializers/default_settings.rb
@@ -30,6 +30,7 @@ AppSettings.defaults['settings.include_ticket_body'] = '1'
 AppSettings.defaults['settings.default_private'] = '0'
 AppSettings.defaults['settings.anonymous_access'] = '0'
 AppSettings.defaults['settings.anonymous_salt'] = 'salt'
+AppSettings.defaults['settings.default_reply_type'] = 'reply'
 
 # Webhook Integrations
 
