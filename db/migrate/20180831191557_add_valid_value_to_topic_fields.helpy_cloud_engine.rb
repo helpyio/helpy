13cc2d7188d6bb0a6b0d638ebd3ff3ba5c6a777f
Goss Nuzzo-Jones <goss@superpedestrian.com>
2019-12-27 17:15:42 -0500

    Revert "adding helpy_cloud stuff to avoid running the install"
    This reverts commit f5b04c933fa02ef6e9761c19907a5619b57c6c6a.

--- db/migrate/20180831191557_add_valid_value_to_topic_fields.helpy_cloud_engine.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from helpy_cloud_engine (originally 20161207213340)
-class AddValidValueToTopicFields < ActiveRecord::Migration
-  def change
-    add_column :topic_fields, :valid_value, :string
-  end
-end
--- db/migrate/20180831191559_add_fields_to_topics.helpy_cloud_engine.rb
+++
@@ -1,9 +0,0 @@
-# This migration comes from helpy_cloud_engine (originally 20170119003113)
-class AddFieldsToTopics < ActiveRecord::Migration
-  change_table :topics do |t|
-    t.string   :condition, default: 'green'
-    t.string   :sentiment
-    t.datetime   :last
-  end
-
-end
