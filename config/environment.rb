141dff5ccfedc888f5e1c248926db51d6f22b53f
scott <scott.miller.utah@gmail.com>
2020-01-02 16:00:28 -0700

    Up version, changelog

--- config/environment.rb
+++ config/environment.rb
@@ -5,6 +5,6 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 # Get the current tag version
-VERSION = '2.7.0'
+VERSION = '2.8.0'
 REVISION = `git log --pretty=format:'%h' -n 1`
 APP_VERSION = "#{VERSION}:#{REVISION}"
