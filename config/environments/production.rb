82fb1608d4b41458e5f1e48484ce480d906c2aa1
Goss Nuzzo-Jones <goss@superpedestrian.com>
2020-01-24 22:00:06 -0500

    try this

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,9 +46,7 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = Logger.const_get(
-    ENV['LOG_LEVEL'] ? ENV['LOG_LEVEL'].upcase : 'INFO'
-  )
+  config.log_level = :debug
   config.logger = Logger.new(STDOUT)
 
   # Prepend all log lines with the following tags.
